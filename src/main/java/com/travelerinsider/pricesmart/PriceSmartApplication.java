1ff429971b3f820196799fa0c239a87f8178600b
Abhishek Roy <royabhishek967@gmail.com>
2026-02-05 13:20:18 +0530

    Initial commit

---
+++ src/main/java/com/travelerinsider/pricesmart/PriceSmartApplication.java
@@ -0,0 +1,13 @@
+package com.travelerinsider.pricesmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceSmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelerinsider/pricesmart/PriceSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelerinsider.pricesmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
